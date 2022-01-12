8dc1911efbd1c2548bbd7ac497a9e5881439a09a
Glib Petrachkov <petrasha@gmail.com>
2022-01-12 20:07:52 +0200

    Добавьте файлы проекта.

---
+++ Cycles_13_January/Cycles_13_January.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Let's learn Cycles! Fast and Furious\n";
+    int fast = 0;
+	while (fast<9)
+	{
+		fast++;
+		cout << "You watch Fast and Furious " << fast << "\n";
+
+	}
+	cout << "Maybe cycles counter is broken)))\n";
+	return 0;
+}
