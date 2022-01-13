8fdca0980dd307056953017268a3c67f2dac2ed4
Oleksandr Zahoruiko <7753892+sunmeat@users.noreply.github.com>
2022-01-13 21:13:22 +0200

    Update Cycles_13_January.cpp

--- Cycles_13_January/Cycles_13_January.cpp
+++ Cycles_13_January/Cycles_13_January.cpp
@@ -1,10 +1,11 @@
-﻿#include <iostream>
+#include <iostream>
 using namespace std;
+
 int main()
 {
-    cout << "Let's learn Cycles! Fast and Furious\n";
-    int fast = 0;
-	while (fast<9)
+        cout << "Let's learn Cycles! Fast and Furious\n";
+        int fast = 0;
+	while (fast < 9) // do not forget extra space characters before and after operators like < :)
 	{
 		fast++;
 		cout << "You watch Fast and Furious " << fast << "\n";
